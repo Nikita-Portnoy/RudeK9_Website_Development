fbdc4fd3019adc917bfdc7f2f43cf69c6225a321
Nikita Portnoy <Nikita.Portnoy.Tech@gmail.com>
2018-06-12 16:51:28 -0700

    Dug, Spelling and General Improvements
    Changed all Google Maps to be of the new embedde type.
    Changed Training page locations section to navigate to the Locations page
    Some further mobile optimazation
    Corrected know spellign mistakes
    Transferred Homepage Main Video to the Dropbox server to improve loading times

--- JS/GMap.js
+++ JS/GMap.js
@@ -1,11 +1,37 @@
+$(document).ready(function(){
+  $('#LocationChino').click(function() {
+    window.location.assign('./Locations.html#CardChino')
+  })
+  $('#LocationOrange').click(function() {
+    window.location.assign('./Locations.html#CardOrange')
+  })
+// Fixing the Static Menu Bar Problem when jumping to ID's
+  var Offset = $(':target').offset();
+  var JumpTo = Offset.top - 60;
+  $('html, body').animate({scrollTop:JumpTo}, 0);
+});
 
 function initMap() {
-  var Chino = {lat: 34.0110535, lng: -117.7325799};
-  var Orange = {lat: 33.8186013, lng: -117.8572332};
+  var Chino = {lat: 34.0110363, lng: -117.7303999};
+  var Orange = {lat: 33.8187024, lng: -117.854874};
 
   var MapChino = new google.maps.Map(document.getElementById('MapChino'), {zoom: 13, center: Chino});
   var MapOrange = new google.maps.Map(document.getElementById('MapOrange'), {zoom: 13, center: Orange});
 
   var MarkerChino = new google.maps.Marker({position: Chino, map: MapChino});
   var MarkerOrange = new google.maps.Marker({position: Orange, map: MapOrange});
+}
+
+function initMapLoc() {
+  var Current = {lat: 33.9217964, lng: -117.7738354};
+  var Chino = {lat: 34.0110363, lng: -117.7303999};
+  var Orange = {lat: 33.8187024, lng: -117.854874};
+
+  var MapLocations = new google.maps.Map(document.getElementById('MapLocations'), {zoom: 11, center: Current});
+
+  $('#LocationOrange').click(function(){MapLocations.panTo(Orange);MapLocations.setZoom(15)});
+  $('#LocationChino').click(function(){MapLocations.panTo(Chino);MapLocations.setZoom(15)});
+
+  var MarkerOrangeLoc = new google.maps.Marker({position: Orange, map: MapLocations});
+  var MarkerChinoLoc = new google.maps.Marker({position: Chino, map: MapLocations});
 }
